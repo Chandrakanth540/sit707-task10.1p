93609a4732ed56ba3ec307c3e0e41a7e72789e93
Chandrakanth540 <104191491+Chandrakanth540@users.noreply.github.com>
2024-05-22 13:36:35 +0530

    Update DateUtilTest.java

--- task3_1P/src/test/java/sit707_tasks/DateUtilTest.java
+++ task3_1P/src/test/java/sit707_tasks/DateUtilTest.java
@@ -17,85 +17,18 @@ public class DateUtilTest {
         Assert.assertNotNull("Student name is ", studentName);
     }
 
-    @Test
-    public void testIncrementLastDayOfJanuary() {
-        DateUtil date = new DateUtil(31, 1, 2021);
-        date.increment();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(2, date.getMonth());
+   @Test
+    public void testAdd() {
+        Assert.assertEquals(8, MathUtils.add(5, 3),0);
+        Assert.assertEquals(0, MathUtils.add(-3, 3),0);
+        Assert.assertEquals(-1, MathUtils.add(-2, 1),0);
     }
 
     @Test
-    public void testIncrementLastDayOfFebruaryLeapYear() {
-        DateUtil date = new DateUtil(29, 2, 2020);
-        date.increment();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(3, date.getMonth());
-    }
-
-    @Test
-    public void testIncrementLastDayOfFebruaryNonLeapYear() {
-        DateUtil date = new DateUtil(28, 2, 2021);
-        date.increment();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(3, date.getMonth());
-    }
-
-    @Test
-    public void testIncrementLastDayOfApril() {
-        DateUtil date = new DateUtil(30, 4, 2021);
-        date.increment();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(5, date.getMonth());
-    }
-
-    @Test
-    public void testIncrementLastDayOfDecember() {
-        DateUtil date = new DateUtil(31, 12, 2021);
-        date.increment();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(1, date.getMonth());
-        Assert.assertEquals(2022, date.getYear());
-    }
-
-    @Test
-    public void testDecrementFirstDayOfMarchLeapYear() {
-        DateUtil date = new DateUtil(1, 3, 2020);
-        date.decrement();
-        Assert.assertEquals(29, date.getDay());
-        Assert.assertEquals(2, date.getMonth());
-    }
-
-    @Test
-    public void testDecrementFirstDayOfMarchNonLeapYear() {
-        DateUtil date = new DateUtil(1, 3, 2021);
-        date.decrement();
-        Assert.assertEquals(28, date.getDay());
-        Assert.assertEquals(2, date.getMonth());
-    }
-
-    @Test
-    public void testDecrementFirstDayOfMay() {
-        DateUtil date = new DateUtil(1, 5, 2021);
-        date.decrement();
-        Assert.assertEquals(30, date.getDay());
-        Assert.assertEquals(4, date.getMonth());
-    }
-
-    @Test
-    public void testDecrementFirstDayOfJanuary() {
-        DateUtil date = new DateUtil(1, 1, 2021);
-        date.decrement();
-        Assert.assertEquals(31, date.getDay());
-        Assert.assertEquals(12, date.getMonth());
-        Assert.assertEquals(2020, date.getYear());
-    }
-
-    @Test
-    public void testDecrementSecondDayOfFebruaryLeapYear() {
-        DateUtil date = new DateUtil(2, 2, 2020);
-        date.decrement();
-        Assert.assertEquals(1, date.getDay());
-        Assert.assertEquals(2, date.getMonth());
+    public void testSub() {
+        Assert.assertEquals(2, MathUtils.sub(5, 3),0);
+        Assert.assertEquals(-6, MathUtils.sub(-3, 3),0);
+        Assert.assertEquals(-3, MathUtils.sub(-2, 1),0);
+        Assert.assertNotNull(MathUtils.add(null, null));
     }
 }
