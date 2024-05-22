b8198468fdfdb59776d91009234b34e0afa4cf96
Chandrakanth540 <104191491+Chandrakanth540@users.noreply.github.com>
2024-05-22 14:19:37 +0530

    Create TaskInbox.java

---
+++ task3_1P/src/main/java/sit707_tasks/TaskInbox.java
@@ -0,0 +1,7 @@
+public class TaskInbox {
+    public String getTaskInbox(String studentId) {
+        return "Task1: Submitted, Feedback: Pending\n" +
+               "Task2: Submitted, Feedback: Given\n" +
+               "Chat: Hi there!, Sure, let me check.";
+    }
+}
